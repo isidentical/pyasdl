2e33668566ebf8174e33cf16a3548e764b6da64c
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2020-06-20 21:36:30 +0300

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup
 
-setup(
-    intall_requires=["pegen==0.0", "typing_extensions"],
-    packages=["pyasdl"],
-    package_data={"pyasdl": ["static/asdl.gram"]},
-)
+setup(intall_requires=["pegen==0.0", "typing_extensions"],)
