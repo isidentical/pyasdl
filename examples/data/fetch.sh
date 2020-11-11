a8ea4851efc948229b51d13a0be9b8a6858ea9fd
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2020-11-11 19:14:11 +0300

    Ability to fetch multiple ASDL files

---
+++ examples/data/fetch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+for version in {6..9};
+    do
+        filename="Python-3$version.asdl";
+        wget "https://raw.githubusercontent.com/python/cpython/3.$version/Parser/Python.asdl" -O $filename;
+        echo -e "-- version=3.$version\n$(cat $filename)" > $filename
+    done;
